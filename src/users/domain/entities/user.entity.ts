cda20c9733dccec7083d261f4eed440dffccb78d
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2024-08-17 17:54:54 -0300

    create user entity test for test contructor method in entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserTypes = {
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+};
+
+export class UserEntity {
+  constructor(public readonly props: UserTypes) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+}
