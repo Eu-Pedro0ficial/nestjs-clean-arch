dfb3aff70408843fe6a83999e92cde3ca66b3ced
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2024-08-18 20:00:59 -0300

    Implements tests for check if email field is validated and implements a new decorator in email field for validate email field with email validator

--- src/users/domain/validators/__tests__/unit/user-validator.spec.ts
+++ src/users/domain/validators/__tests__/unit/user-validator.spec.ts
@@ -42,12 +42,50 @@ describe('User validator unit tests', () => {
         'name must be shorter than or equal to 255 characters',
       ]);
     });
+  });
 
-    it('valid case for name field', () => {
-      const props = UserDataBuilder({});
-      const isValid = sut.validate(props);
-      expect(isValid).toBeTruthy();
-      expect(sut.validatedData).toStrictEqual(new UserRules(props));
+  describe('Email field', () => {
+    it('Invalidations cases for email field', () => {
+      let isValid = sut.validate(null);
+      expect(isValid).toBeFalsy();
+      expect(sut.erros['email']).toStrictEqual([
+        'email should not be empty',
+        'email must be an email',
+        'email must be a string',
+        'email must be shorter than or equal to 255 characters',
+      ]);
+
+      isValid = sut.validate({ ...UserDataBuilder({}), email: '' });
+      expect(isValid).toBeFalsy();
+      expect(sut.erros['email']).toStrictEqual([
+        'email should not be empty',
+        'email must be an email',
+      ]);
+
+      isValid = sut.validate({ ...UserDataBuilder({}), email: 10 as any });
+      expect(isValid).toBeFalsy();
+      expect(sut.erros['email']).toStrictEqual([
+        'email must be an email',
+        'email must be a string',
+        'email must be shorter than or equal to 255 characters',
+      ]);
+
+      isValid = sut.validate({
+        ...UserDataBuilder({}),
+        email: 'a'.repeat(260) as any,
+      });
+      expect(isValid).toBeFalsy();
+      expect(sut.erros['email']).toStrictEqual([
+        'email must be an email',
+        'email must be shorter than or equal to 255 characters',
+      ]);
     });
+  });
+
+  it('valid case for user validator class', () => {
+    const props = UserDataBuilder({});
+    const isValid = sut.validate(props);
+    expect(isValid).toBeTruthy();
+    expect(sut.validatedData).toStrictEqual(new UserRules(props));
   });
 });
