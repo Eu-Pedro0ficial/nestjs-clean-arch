0c7419b84ade50382720a82bdeeeda5c620298a0
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2024-08-25 13:07:03 -0300

    created user reporisoty in memory and implements methods for find by email and check email exists

---
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -0,0 +1,25 @@
+import { ConflictError } from '@/shared/domain/erros/conflict_error';
+import { NotFoundError } from '@/shared/domain/erros/not-fount-error';
+import { InMemoryRepository } from '@/shared/domain/repositories/in-memory.repository';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+
+export class UserInMemoryRepository
+  extends InMemoryRepository<UserEntity>
+  implements UserRepository
+{
+  async findByEmail(email: string): Promise<UserEntity> {
+    const __email = `${email}`;
+    const entity = this.items.find(item => item.id === __email);
+    if (!entity)
+      throw new NotFoundError(`Entity not found using email ${__email}`);
+    return entity;
+  }
+
+  async emailExits(email: string): Promise<void> {
+    const __email = `${email}`;
+    const entity = this.items.find(item => item.id === __email);
+
+    if (entity) throw new ConflictError(`Email address already used`);
+  }
+}
