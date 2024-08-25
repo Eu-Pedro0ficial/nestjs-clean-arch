30747d08d89058cb7981c2ba18fd41207fac8e59
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2024-08-25 10:38:45 -0300

    refatoring tests for check if validated method is run was the class is instanced and called user update and password update

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -5,12 +5,13 @@ describe('User entity unit tests', () => {
   let props: UserTypes;
   let sut: UserEntity;
   beforeAll(() => {
+    UserEntity.validate = jest.fn();
     props = UserDataBuilder({});
-
     sut = new UserEntity(props);
   });
 
   it('Constructor method', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.name).toEqual(props.name);
     expect(sut.props.email).toEqual(props.email);
     expect(sut.props.password).toEqual(props.password);
@@ -53,11 +54,13 @@ describe('User entity unit tests', () => {
   });
 
   it('Should update a user', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
     sut.update('other name');
     expect(sut.props.name).toEqual('other name');
   });
 
   it('Should update a password field', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
     sut.updatePassword('other password');
     expect(sut.props.password).toEqual('other password');
   });
